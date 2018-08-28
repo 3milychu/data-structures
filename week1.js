10e84a0599199fa8774fb7db72399dd3d7a7d1b9
EC2 Default User <ec2-user@ip-172-31-19-57.us-east-2.compute.internal>
2018-08-28 01:52:33 +0000

    push

--- week1.js
+++ week1.js
@@ -6,27 +6,33 @@ var fs = require('fs');
 
 // request('https://parsons.nyc/thesis-2018/', function(error, response, body){
 //     if (!error && response.statusCode == 200) {
-//         fs.writeFileSync('/home/ec2-user/environment/data/thesis.txt', body);
+//         fs.writeFileSync('/home/ec2-user/environment/data/thesis2.txt', body);
 //     }
 //     else {console.log("Request failed!")}
 // });
 
+
 var entries = 11;
 var i;
-for(i=0;i<entries.length;i++) {
-    if(i<10) {
-            request('https://parsons.nyc/aa/m0'+i, function(error, response, body){
-        if (!error && response.statusCode == 200) {
-            fs.writeFileSync('/home/ec2-user/environment/data/thesis'+i+".txt", body);
-        }
-        else {console.log("Request failed!")}
-    } else {
-         request('https://parsons.nyc/aa/m'+i, function(error, response, body){
-        if (!error && response.statusCode == 200) {
-            fs.writeFileSync('/home/ec2-user/environment/data/thesis'+i+".txt", body);
-        }
-        else {console.log("Request failed!")}
-    }
-});
+
+function format(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
+
+for(i=1;i<entries;i++) {
+    var list = []
+    var url = 'https://parsons.nyc/aa/m'+ format(i) +".html";
+    list.push(url);
     
+    for(i=0;i<list.length;i++){
+        console.log("Requesting"+ list[i]);
+        request(list[i], function(error, response, body){
+    if (!error && response.statusCode == 200) {
+        var path = "/home/ec2-user/environment/data/thesis" + list[i] + ".txt"
+        console.log(path);
+        fs.writeFileSync(path, body);
+    }
+    else {console.log("Request failed!")}
+    })
+};
 }
