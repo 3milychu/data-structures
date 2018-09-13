5aef768c8223402a48b9b5d8dcab0433dc87dd06
EC2 Default User <ec2-user@ip-172-31-19-57.us-east-2.compute.internal>
2018-09-13 14:41:26 +0000

    push

--- week1.js
+++ week1.js
@@ -4,35 +4,44 @@
 var request = require('request');
 var fs = require('fs');
 
-// request('https://parsons.nyc/thesis-2018/', function(error, response, body){
-//     if (!error && response.statusCode == 200) {
-//         fs.writeFileSync('/home/ec2-user/environment/data/thesis2.txt', body);
-//     }
-//     else {console.log("Request failed!")}
-// });
-
-
 var entries = 11;
 var i;
 
+// Format numbers so 0= 00, 1=01, 10= 10
 function format(n) {
     return (n < 10) ? ("0" + n) : n;
 }
 
-for(i=1;i<entries;i++) {
-    var list = []
-    var url = 'https://parsons.nyc/aa/m'+ format(i) +".html";
-    list.push(url);
-    
-    for(i=0;i<list.length;i++){
-        console.log("Requesting"+ list[i]);
-        request(list[i], function(error, response, body){
-    if (!error && response.statusCode == 200) {
-        var path = "/home/ec2-user/environment/data/thesis" + list[i] + ".txt"
-        console.log(path);
-        fs.writeFileSync(path, body);
-    }
-    else {console.log("Request failed!")}
-    })
-};
+// Testing the format of the path construction
+// for (var i=1;i<entries;i++){
+//     var path = "https://parsons.nyc/aa/m" + format(i) +".html";
+//     console.log(path);
+// }
+
+// Construct a function for each path and store
+var funcs = [];
+
+function getEachPage(i) {
+    return function() {
+        var path = "https://parsons.nyc/aa/m" + format(i) +".html";
+        request(path, function(error,response,body){
+         if(!error & response.statusCode == 200){
+             var save_path = '/home/ec2-user/environment/data/m' + format(i)+ '.txt';
+             fs.writeFileSync(save_path, body);
+             console.log(path + " saved successfully!");
+         } else {
+             console.log("Request failed");
+         }
+        })
+ }
+}
+
+// Create the functions for each page
+for (var i=1; i<entries; i++) {
+    funcs[i] = getEachPage(i);
+}
+
+// Run each function
+for (var j=1;j<entries; j++) {
+  funcs[j]();                     
 }
