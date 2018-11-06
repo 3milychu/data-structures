ff3f7323e5e93de0e0b34d72919fdbbf4545c2d0
Emily <3milychu@gmail.com>
2018-11-06 00:11:41 +0000

    push data to postgresql db

--- week6-7/sql.js
+++ week6-7/sql.js
@@ -12,39 +12,48 @@ db_credentials.database = 'aadb';
 db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
-// // Connect to the AWS RDS Postgres database
-// const client = new Client(db_credentials);
-// client.connect();
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
 
 // Sample SQL statement to create a table: 
 // Sample SQL statement to delete a table: 
 // var thisQuery = "DROP TABLE aadata;"; 
+// var thisQuery = "CREATE TABLE aadata (time_day varchar(25), time_start varchar(25), time_end varchar(25));";
 // Sample SQL statement to query the entire contents of a table: 
 // var thisQuery = "SELECT * FROM aadata;";
+var thisQuery = "SELECT address FROM aadata;";
 // var thisQuery = "SELECT address FROM aalocations;";
 // var thisQuery = "SELECT lng FROM aalocations;";
-// var thisQuery = "CREATE TABLE aadata (day varchar(25), start varchar(25), end varchar(25), name varchar(75), location varchar(75), address varchar(75), lat double precision, long double precision, region varchar(75), type varchar(150), interests varchar(150));";
+// var thisQuery = "CREATE TABLE aadata (time_day varchar(25), time_start varchar(25), time_end varchar(25), name varchar(75), location varchar(75), address varchar(75), lat double precision, long double precision, region varchar(75), type varchar(150), interests varchar(150));";
 // var thisQuery = "CREATE TABLE aadata (time_day varchar(25), time_start varchar(25), time_end varchar(25), name varchar(75));";
 
-// client.query(thisQuery, (err, res) => {
-//     console.log(err, res);
-//     client.end();
-// });
+client.query(thisQuery, (err, res) => {
+    console.log(err, res);
+    client.end();
+});
 
-var file = 1;
-var json = fs.readFileSync("data/sql_m0" + file + ".json");
-var data = JSON.parse(json);
+// function format(n) {
+//     return (n < 10) ? ("0" + n) : n;
+// }
 
-async.eachSeries(data, function(value, callback) {
-    const client = new Client(db_credentials);
-    client.connect();
 
-    var thisQuery = "INSERT INTO aadata VALUES (E'" + value.day + "', " + value.start + ", " + value.end + ", " + value.name + ", " + value.location + ", " + value.address + ", " + value.lat + ", " + value.long + ", " + value.region + ", " + value.type + ", " + value.interest +");";
-    // var thisQuery = "SELECT time_day, time_start, time_end, name, location FROM aadata WHERE day = 'Monday' and mtghour == '7:00 PM';";
+// for (var i=1;i<11;i++){
+//     var file = format(i);
+//     var json = fs.readFileSync("data/sql_m" + file + ".json");
+//     var data = JSON.parse(json);
+
+// async.eachSeries(data, function(value, callback) {
+//     const client = new Client(db_credentials);
+//     client.connect();
     
-    client.query(thisQuery, (err, res) => {
-        console.log(err, res);
-        client.end();
-    });
-    setTimeout(callback, 1000); 
-}); 
+//     var thisQuery = "INSERT INTO aadata VALUES (E'" + value.day + "', '" + value.start + "', '" + value.end + "', '" + value.name + "', '" + value.location + "', '" + value.address + "', '" + value.lat + "', '" + value.long + "', '" + value.region + "', '" + value.type + "', '" + value.interest + "');";
+   
+//     client.query(thisQuery, (err, res) => {
+//         console.log(err, res);
+//         client.end();
+//     });
+//     setTimeout(callback, 1000); 
+// }); 
+
+// };
