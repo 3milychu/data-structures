eef9cd328597221a268d79848c17fb3f81e9bf70
Emily <3milychu@gmail.com>
2018-12-02 03:04:10 +0000

    update

--- week10/server.js
+++ week10/server.js
@@ -17,25 +17,32 @@ AWS.config.accessKeyId = process.env.AWS_ID;
 AWS.config.secretAccessKey = process.env.AWS_KEY;
 AWS.config.region = "us-east-1";
 
-
 var aahtml1 = `
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
- <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
    integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
    crossorigin=""/>
+   <style>
+   #map {
+       height:100vh;
+   }
+   </style>
 </head>
 <body>
 
 <div id="map"></div>
-</body>
-<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+ <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
    integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
    crossorigin=""></script>
+</body>
 <script>
-function mapdata() {
+var data;
+var map_data;
+
+function getdata(callback) {
  Array.prototype.groupBy = function (props) {
    var arr = this;
    var partialResult = {};
@@ -64,13 +71,15 @@ function mapdata() {
    
    return finalResult;
 }
-var data =`
+data =`
 var aahtml2 = `
 ;
-var map_data = data.groupBy(['lat','long']);
+map_data = data.groupBy(['lat','long']);
 console.log(map_data);
-
-var mymap = L.map('map').setView([40.734636,-73.994997], 13);
+callback(map_data);
+}
+function getmap(data) {
+    var mymap = L.map('map').setView([40.734636,-73.994997], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -79,13 +88,13 @@ var mymap = L.map('map').setView([40.734636,-73.994997], 13);
         accessToken: 'pk.eyJ1Ijoidm9ucmFtc3kiLCJhIjoiY2pveGF0aDV2MjIyOTNsbWxlb2hhMmR4dCJ9.JJdYD_jWgRwUeJkDWiBz3w'
     }).addTo(mymap);
     for (var i=0; i<map_data.length; i++) {
-        console.log(map_data[i].lat + ", " + map_data[i].long);
-        L.marker( [map_data[i].lat, map_data[i].long] ).bindPopup(JSON.stringify(map_data[i].meetings)).addTo(mymap);
-    }   
+        ref = map_data[i];
+        console.log(ref.values);
+        L.marker( [map_data[i].lat, map_data[i].long] ).bindPopup(ref.values.length + ": " + JSON.stringify(map_data[i].values)).addTo(mymap);
+    }  
 }
 
-mapdata();
-
+getdata(getmap);
 </script>
 </html>
 `
@@ -93,13 +102,21 @@ app.get('/aameetings', function(req, res) {
     
     // Connect to the AWS RDS Postgres database
     const client = new Pool(db_credentials);
-    
-    var thisQuery = `SELECT * FROM aadata`;
+
+var selected_day = "Monday";
+var selected_start_time = "7:00 AM";
+
+var thisQuery = `SELECT * FROM aadata`;
+// var thisQuery = `SELECT lat, long, json_agg(json_build_object('loc', location, 'address', address, 'time_start', time_start, 'name', name, 'day', time_day, 'type', type, 'time_end', time_end)) as meetings
+//              FROM aadata
+//              WHERE day = ` + selected_day + 
+//              `GROUP BY lat, long
+//              ;`;
 
     client.query(thisQuery, (qerr, qres) => {
         if (qerr) { throw qerr }
         else {
-            var resp = aahtml1 + JSON.stringify(qres.rows) + aahtml2
+            var resp = aahtml1 + JSON.stringify(qres.rows) + aahtml2;
             res.send(resp);
             client.end();
             console.log('2) responded to request for aa meeting data');
